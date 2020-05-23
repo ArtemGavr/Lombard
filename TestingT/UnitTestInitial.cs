3608587d7cb668f491182864ef5f14e4b41ef740
ArtemGavr <58862869+ArtemGavr@users.noreply.github.com>
2020-05-23 18:10:42 +0300

    Added testing

---
+++ TestingT/UnitTestInitial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Lombard_Project.UserClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestingT
+{
+    [TestClass]
+    public class UnitTestInitial
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Lombard lombard = new Lombard();
+            lombard.FillTestData(10);
+            Assert.AreEqual("Product0", lombard.Products[0].Name);
+        }
+    }
+}
